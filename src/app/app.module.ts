4e2894d556379f1b469d9ab4c5caa8155b7138fd
Cong <minhcongvghy@gmail.com>
2019-10-28 09:58:44 +0700

    [Bài tập] Ứng dụng todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 import { BlogComponent } from './blog/blog.component';
 import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogComponent,
     BlogDetailComponent,
-    BlogEditComponent
+    BlogEditComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
